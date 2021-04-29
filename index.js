32e11fa38ee637426a035ccaccb0a0de031a9999
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-29 21:19:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+function superbowlWin(record){
+  
+ let result = record.find( record => record.result === "W" )
+  return  !!result ? result.year : undefined 
 }
+  
+
