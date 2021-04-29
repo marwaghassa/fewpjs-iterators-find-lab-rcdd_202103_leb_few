5a2070bf10a813bcabd3493a2d3a2e8bfb2c5531
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-29 21:03:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function testFunc() {
   return "hi"
 }
 
-function superbowlWin(){
-  
-  
-}
+function superbowlWin(records){
+  records.find(record =>record.result=="w")
+  for(let i=0;i<record.length;i++){
+    if(){
+    
+  return }
+}}
